55304de2ee775d9813798d6d9ab3930fdfa8a6b2
AnukulPK <anukulpk2@gmail.com>
2022-07-16 15:50:27 +0530

    added user page wip

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   };
 
@@ -31,6 +32,28 @@ export const GithubProvider = ({ children }) => {
       type: "GET_USERS",
       payload: items,
     });
+  };
+
+  // get individual user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
   };
 
   //Clear Users from state
@@ -62,7 +85,9 @@ export const GithubProvider = ({ children }) => {
     <GithubContext.Provider
       value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
+        getUser,
         searchUsers,
         clearUsers,
       }}
