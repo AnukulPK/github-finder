7a178c85dfc079ad5d7ac9f1583084504f684a2c
AnukulPK <anukulpk2@gmail.com>
2022-07-18 10:45:43 +0530

    added user repo api

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
@@ -56,6 +57,31 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  //Get User repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
   //Clear Users from state
   const clearUsers = () => {
     dispatch({
@@ -87,9 +113,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         user: state.user,
         loading: state.loading,
+        repos: state.repos,
         getUser,
         searchUsers,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
